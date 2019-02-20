ceec35bd130737522663e7378cdc81d45ef1aedc
PernillaQ <31962960+PernillaQ@users.noreply.github.com>
2019-02-20 11:39:29 +0100

    render posts

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -1,19 +1,19 @@
-//import firebase from 'firebase';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-//import * as firebase from 'firebase/app';
+// import firebase from 'firebase';
+import firebase from 'firebase/app'
+import 'firebase/auth'
+// import * as firebase from 'firebase/app';
 
 // Initialize Firebase
 var config = {
-  apiKey:   /* process.env.FIREBASE_APIKEY*/,
-  authDomain: "qvist-jewellery.firebaseapp.com",
-  databaseURL: "https://qvist-jewellery.firebaseio.com",
-  projectId: "qvist-jewellery",
-  storageBucket: "qvist-jewellery.appspot.com",
-  messagingSenderId: "520070195490"
-};
-firebase.initializeApp(config);
-/*class Firebase {
+  apiKey:  /* process.env.FIREBASE_APIKEY */,
+  authDomain: 'qvist-jewellery.firebaseapp.com',
+  databaseURL: 'https://qvist-jewellery.firebaseio.com',
+  projectId: 'qvist-jewellery',
+  storageBucket: 'qvist-jewellery.appspot.com',
+  messagingSenderId: '520070195490'
+}
+firebase.initializeApp(config)
+/* class Firebase {
   constructor(){
     firebase.initializeApp(config);
     // behövs bara göras 1 gång.
@@ -22,12 +22,12 @@ firebase.initializeApp(config);
   signIn = (email, password) => {
     return this.auth.signInWithEmailAndPassword(email, password);
   }
-}*/
+} */
 // skapa instansen av firebase..för att instansiering inte ska behöva ske flera gånger..
 // detta kommer vi använda
-//const api = new Firebase();
+// const api = new Firebase();
 
-//export default api;
-export const auth = firebase.auth();
+// export default api;
+export const auth = firebase.auth()
 
-export default firebase;
+export default firebase
--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -8,27 +8,40 @@ import TheDesert from './TheDesert/theDesert.js'
 
 class Collections extends Component {
   state = {
-    allPosts:[]
-    // containing postArray[] and postCollections[]
+    allCityPosts:[],
+    allDesertPosts:[]
   }
+  
   //get all posts
   componentWillMount(){
-      firebase.database().ref(`collections/thecity`).on('value', (snapshot) => {
-      const allPosts = this.toArray(snapshot.val());
-      this.setState({allPosts: allPosts})
-      //this.props.people.map(person => { value: person.id, text: person.name })
-      /*const allPostCollections = this.toArray(snapshot.val())
-      this.setState({allCollections:allPostCollections})*/
+      firebase.database().ref(`collections`).on('value', (snapshot) => {
+      this.toArray(snapshot.val());
     })
   }
 
  toArray = (firebaseObject) => {
-   let array = []
+   let city = []
+   let desert = []
+
    for(let item in firebaseObject){
-     console.log(firebaseObject[item])
-   array.push({ key: item, value: firebaseObject[item] })
+     if (item === 'thecity'){
+        let theCityValue = firebaseObject[item]
+
+        for(let x in theCityValue){
+        city.push({ key: x, value: theCityValue[x] })
+        }
+     }
+     else if (item === 'thedesert'){
+       let theDesertValue = firebaseObject[item]
+
+       for(let x in theDesertValue){
+       desert.push({ key: x, value: theDesertValue[x] })
+       }
+     }
    }
-   return array
+   this.setState({allCityPosts:city, allDesertPosts:desert})
+   console.log(this.state.allCityPosts);
+   console.log(this.state.allDesertPosts);
  }
 
 
@@ -65,18 +78,23 @@ class Collections extends Component {
   const { theCity, theDesert } = this.props
   //console.log( this.state.allPosts[0])//array
   //console.log(this.state.allPosts.postCollections)
-  let list = this.state.allPosts.map(post =>
+  let cityList = this.state.allCityPosts.map(post =>
   <div className="post" key={post.key}>
     <img src={post.value.url}alt="a Piece of jewellery"/>
-    <h2>{post.value.title}</h2>
+    <h3>{post.value.title}</h3>
     <p>{post.value.content}</p>
-
+  </div>)
+  let desertList = this.state.allDesertPosts.map(postD =>
+  <div className="post" key={postD.key}>
+    <img src={postD.value.url}alt="a Piece of jewellery"/>
+    <h3>{postD.value.title}</h3>
+    <p>{postD.value.content}</p>
   </div>)
     return (
       <div className='Collections-wrapper' id='collections'>
         <div className='Collections-hexagon'>
-{list}
           <div className='Collections-theCity-wrapper'>
+          {cityList}
             <img src={bangle} className='Collections-bangle' alt='bangle' />
             <h3 id='theCity'>The city</h3>
             <p>Birch bark Crafts has been found in Swedn since Thousands of years thanks to the
@@ -88,6 +106,7 @@ class Collections extends Component {
           </svg>
           <h2>Collections</h2>
           <div className='Collections-theDesert-wrapper'>
+              {desertList}
             <img src={earcuffs} className='App-earcuff' alt='earcuffs' />
             <h3 id='theDesert'>The desert</h3>
             <p>The Desert collections grew out of the very depth of desires, a yearning to
